02bec8817923e43e66e35da6872d7d09498f51e5
Paulo Henrique <paulo.henriquepm@outlook.com>
2023-04-11 18:36:18 -0300

    missing required fields implementation

--- node/src/create-user-service.ts
+++ node/src/create-user-service.ts
@@ -1,3 +1,16 @@
-class CreateUserService {}
+class CreateUserService {
+  perform() {
+    return {
+      success: false,
+      errors: {
+        name: 'blank',
+        email: 'blank',
+        password: 'blank',
+        password_confirmation: 'blank',
+      },
+      data: null,
+    }
+  }
+}
 
 export { CreateUserService }
