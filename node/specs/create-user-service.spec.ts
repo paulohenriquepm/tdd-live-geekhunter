851e7521860bb3c7499e7ef42592cce4cc402664
Paulo Henrique <paulo.henriquepm@outlook.com>
2023-04-11 18:45:08 -0300

    minimum length password spec

--- node/specs/create-user-service.spec.ts
+++ node/specs/create-user-service.spec.ts
@@ -3,7 +3,7 @@ import { CreateUserService } from '@src/create-user-service'
 describe('CreateUserService', () => {
   describe('when all required fields are provided', () => {
     describe('but the name length is less than 3', () => {
-      it('return a minimum length error at name field', () => {
+      it('returns a minimum length error at name field', () => {
         const createUserService = new CreateUserService()
 
         const result = createUserService.perform({
@@ -17,6 +17,27 @@ describe('CreateUserService', () => {
           success: false,
           errors: {
             name: 'minimum_length',
+          },
+          data: null,
+        })
+      })
+    })
+
+    describe('but the password length is less than 8', () => {
+      it('returns a minimum length error at password field', () => {
+        const createUserService = new CreateUserService()
+
+        const result = createUserService.perform({
+          name: 'John',
+          email: 'email@email.com',
+          password: '1234567',
+          password_confirmation: '1234567',
+        })
+
+        expect(result).toMatchObject({
+          success: false,
+          errors: {
+            password: 'minimum_length',
           },
           data: null,
         })
